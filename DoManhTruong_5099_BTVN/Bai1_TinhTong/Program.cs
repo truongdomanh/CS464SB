973fd9dd517d6d54578134aeaba8c081c5420dd9
truongdomanh1906 <truongdomanh1906@gmail.com>
2023-07-03 17:46:19 +0700

    test lan dau

---
+++ DoManhTruong_5099_BTVN/Bai1_TinhTong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_TinhTong
+{
+    class Program
+    {
+        static int Nhap()
+        {
+            int n;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Moi nhap n");
+            n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        static int Tong(int n)
+        {
+            int s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s += i;
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            n = Nhap();
+            Console.WriteLine("Tong tu 1 den {0} la {1}", n, Tong(n));
+            Console.ReadLine();
+        }
+    }
+}
